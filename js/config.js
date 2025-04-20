dc0e134c17e2e3f79002ae458559753c7466f6f4
hi-asimahmed <hi.asimahmed@gmail.com>
2025-04-20 15:07:35 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_5I6RO64pF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1kfl8n7htja96o4a7up5osuvpl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'l90s5rto3d5jicml9mhipnioo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
